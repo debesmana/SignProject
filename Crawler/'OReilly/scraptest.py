4bb66f9ad597684ae277bef97266672291a9e1ca
Alma <debesmana@gmail.com>
2017-01-18 14:57:05 +0000

    added beautifulsoup
    extracts h1 from html

--- Crawler/'OReilly/scraptest.py
+++ Crawler/'OReilly/scraptest.py
@@ -1,3 +1,11 @@
 from urllib2 import urlopen
-html = urlopen("http://www.dainuskapis.lv/katalogs/1.-Par-dziesmam-un-dziedasanu")
-print(html.read())
+from bs4 import BeautifulSoup
+#importing the libraries that will be used
+#Beautiful soup can be found here : https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup
+
+html = urlopen("http://www.pythonscraping.com/pages/page1.html")
+#The website being scraped (looked at)
+bsObj = BeautifulSoup(html.read())
+#turns the html into a bs object (makes it easier to read)
+print(bsObj.html.body.h1)
+#fetches the html object called (in this case h1)
