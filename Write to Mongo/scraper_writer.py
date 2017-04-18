34e3e0574a6f45247ddcc4b00e3bcd184ced2ebd
Alma <debesmana@gmail.com>
2017-04-18 14:01:15 +0200

    works on localhost (working on getting it to work on server)

--- Write to Mongo/scraper_writer.py
+++ Write to Mongo/scraper_writer.py
@@ -8,7 +8,7 @@ import re
 import math
 import itertools
 from bs4 import BeautifulSoup
-from pymongo import MongoClient
+import pymongo
 #importing the libraries that will be used
 #Beautiful soup can be found here : https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup
 
@@ -63,11 +63,11 @@ def get_daina(url):
             except UnicodeError:
                 print "string is not UTF-8"
             #turns html data into string
-            daina_string = re.sub(r"<[A-Za-z0-9\.\:\"\'\=\;\-\/ \t]+>", "", daina_string)
+            daina_string = re.sub(ur"<[A-Za-z0-9\.\:\"\'\=\;\-\/ \t]+>", "", daina_string)
             #regex to get rid of html elements
-            daina_string = re.sub(r"[0-9]+-[0-9]+", "", daina_string)
+            daina_string = re.sub(ur"[0-9]+-[0-9]+", "", daina_string)
             #regex to get rid of numbering at begining of poem
-            daina_string = re.sub(r"[ \n\t+]", " ", daina_string)
+            daina_string = re.sub(ur"[ \n\t+]", " ", daina_string)
             #regex to get rid of trailing white space, tabs and new lines
             #print "start of n", daina_string
             #test if values are right
@@ -77,37 +77,69 @@ def get_daina(url):
     except AttributeError as e:
         print e
         return None
-    
-def flatten_list(a_list):
-    """Flattens lists"""
-    merged = list(itertools.chain.from_iterable(a_list))
-    return merged
+def flatten_list(listOfLists):
+    """Flatten a list of (lists of (lists of strings)) for any level 
+    of nesting"""
+    #http://stackoverflow.com/questions/17864466/flatten-a-list-of-strings-and-lists-of-strings-and-lists-in-python
+    merged_list = []
+
+    for i in listOfLists:
+        # Only append if i is a basestring (superclass of string)
+        if isinstance(i, basestring):
+            merged_list.append(i.decode("utf-8"))
+        # Otherwise call this function recursively
+        else:
+            merged_list.extend(flatten_list(i))
+    #print merged_list
+    return merged_list
 #writes to mongo
+def get_words(text):
+    word_list = []
+    for x in range(len(text)):
+        word_list = re.findall(r'(?u)\w+', text)[x].encode('utf-8')
+        print word_list
 def write_to_db(daina): 
     """Writes poems to db"""
-    pass
-    #setting up local mongoDB
-    client = MongoClient()
+    # Connection to Mongo DB
+    try:
+        #setting up link to mongoDB
+        conn = pymongo.MongoClient('138.68.170.30', 27067)
+        #conn = pymongo.MongoClient('localhost', 27017) #Localhost to test if problem with server
+        print "Connected successfully!!!"
+    except pymongo.errors.ConnectionFailure, e:
+        print "Could not connect to MongoDB: %s" % e
     #if a database (or collection) with a certain name isn't found mongo creates a new one when the first document is inserted
-    db = dainas.test
-    collection = db.test_collection
-    post = {"poem": daina}
-    db.test_collection.insert(post)
+    # Define my mongoDB database
+    db = conn.poems
+    # Define my collection where I'll insert my search
+    posts = db.posts
+    for x in range(len(daina)):
+        # Empty dictionary for storing poems
+        data = {}
+        data['daina'] = daina[x]
+        posts.insert(data)
+        print data
+    print posts.count()
+
+
 
 #functions are called here
-
 
 get_links()
 for x in range(len(url_list)):
     get_daina(url_list[x])
-
 for x in range(len(daina_list)): 
-    print "poem %s" %(x), daina_list[x] 
-    #print "poem %s" %(x), repr(daina_list[x])
+    #print "poem %s" %(x), daina_list[x] unflattened list
+    #print "poem %s" %(x), repr(daina_list[x]) checing unicode 
+    merged_daina_list = flatten_list(daina_list)
+    #print "poem %s" %(x), merged_daina_list [x]
 for x in range(len(word_list)):
     print "word %s" %(x), word_list[x]
 
-counter_file.close()
+write_to_db(merged_daina_list)
+
+#get_words(merged_daina_list)
+counter_file.close()#files that are opened need to be closed
 #prints the poem
 # TODO: write each daina object to mongoDB
 # TODO: write each word to mongoDB
--- basic_word_splitter.py
+++ basic_word_splitter.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import re
 
-text = u"Māsa mnūŗš ļķī ēŗtāņw"
-def split_string(string):
+text = [u"Māsa mani kaitināja ļķī ēŗtāņw","Māsa mani kaitināja ļķī ēŗtāņw"]
+def split_string(poem):
+    string = " ".join(text)
     word_list = []
     for x in range(len(text.split())):
         word_list = re.findall(r'(?u)\w+', string)[x].encode('utf-8')
